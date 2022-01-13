1261b39a222e7f91ca485161bba78e4a9d2ae57f
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-01-13 14:49:17 +0900

    refactor test

--- test/test_utils.py
+++ test/test_utils.py
@@ -19,6 +19,7 @@ from dictos.utils import (
 )
 
 _stencil_half_width = 20  # up to 20th order accuracy
+_max_symbol_length = 10
 
 
 def random_string(len):
@@ -53,18 +54,19 @@ def random_int(min, max, exclude=None):
 
 
 class UtilsTest(unittest.TestCase):
-    def test_create_set_of_coordinate_symbols_from_stencil(self):
+    def test_create_coordinate_symbols(self):
         """
-        test suite for utils.create_set_of_coordinate_symbols_from_stencil.
-        - it returns [nh] when [n] is passed.
-        - it returns [ah bh ch ...] when [a b c ...] is passed.
-        - it returns [adx bdx cdx ...] when [a b c ...] and 'dx' are passed.
+        test suite for utils.create_coordinate_symbols.
+        1. it returns [n*h] when [n] is passed.
+        2. it returns [a*h b*h c*h ...] when [a b c ...] is passed.
+        3. it returns [a*dx b*dx c*dx ...] when [a b c ...] and 'dx' are passed.
+        4. it raise error when empty list is passed.
+        5. it raise error when at least a number in the stencil appears more than once.
         """
 
-        num = list(range(-_stencil_half_width, 0)) + list(
-            range(1, _stencil_half_width + 1)
-        )
-        random.shuffle(num)
+        # subtest 1
+        # it returns [n*h] when [n] is passed.
+        num = random_int(-_stencil_half_width, _stencil_half_width, exclude=[0])
         for n in num:
             with self.subTest(n):
                 stencil = [n]
@@ -74,8 +76,9 @@ class UtilsTest(unittest.TestCase):
                 acctual = [n * h]
                 self.assertEqual(expected, acctual)
 
-        num = list(range(1, _stencil_half_width + 1))
-        random.shuffle(num)
+        # subtest 2
+        # it returns [a*h b*h c*h ...] when [a b c ...] is passed.
+        num = random_int(1, _stencil_half_width)
         for n in num:
             with self.subTest(n):
                 stencil = [i for i in range(n)]
@@ -95,8 +98,9 @@ class UtilsTest(unittest.TestCase):
         #     with self.assertRaises(Exception):
         #         create_coordinate_symbols([1, 1])
 
-        num = list(range(1, _stencil_half_width + 1))
-        random.shuffle(num)
+        # subtest 3
+        # it returns [a*dx b*dx c*dx ...] when [a b c ...] and 'dx' are passed.
+        num = random_int(1, _stencil_half_width)
         for n in num:
             with self.subTest(n):
                 interval = random_string(random.randint(1, 10))
