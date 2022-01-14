d81a0f705b381a769d0bbbe7afe57cb1d8783a97
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-01-14 22:42:29 +0900

    refactor variable names

--- dictos/interpolation.py
+++ dictos/interpolation.py
@@ -13,42 +13,46 @@ from .taylor_expansion import taylor_series
 
 
 def equation(stencil, same_subscripts_as_stencil=False):
-    xSet = create_coordinate_symbols(stencil, DEFAULT_INTERVAL)
-    fSet = create_function_symbols(xSet, DEFAULT_FUNCTION, same_subscripts_as_stencil)
+    x_set = create_coordinate_symbols(stencil, DEFAULT_INTERVAL)
+    f_set = create_function_symbols(x_set, DEFAULT_FUNCTION, same_subscripts_as_stencil)
 
     coef = coefficients(stencil)
 
-    return sp.simplify(sum([coef[i] * fSet[i] for i in range(len(fSet))]))
+    return sp.simplify(sum([coef[i] * f_set[i] for i in range(len(f_set))]))
 
 
 def coefficients(stencil, as_numr_denom=False):
-    xSet = create_coordinate_symbols(stencil, DEFAULT_INTERVAL)
-    fSet = create_function_symbols(xSet, DEFAULT_FUNCTION)
+    x_set = create_coordinate_symbols(stencil, DEFAULT_INTERVAL)
+    f_set = create_function_symbols(x_set, DEFAULT_FUNCTION)
 
     x = sp.symbols(DEFAULT_INDEPENDENT_VARIABLE)
 
-    doe = len(xSet) - 1
-    eq = sum([lagrangian_basis(x, doe, i, xSet) * fSet[i] for i in range(doe + 1)])
+    num_set = len(x_set)
+    degree = num_set - 1
+    eq = sum([lagrangian_basis(x, degree, i, x_set) * f_set[i] for i in range(num_set)])
 
-    num, den = sp.simplify(eq.subs(x, 0)).as_numer_denom()
-    num_coef = num.as_poly(fSet).coeffs()
-    den_coef = den.as_poly(fSet).coeffs()
+    numr, denom = sp.simplify(eq.subs(x, 0)).as_numer_denom()
+    numr_coef = numr.as_poly(f_set).coeffs()
+    denom_coef = denom.as_poly(f_set).coeffs()
 
-    coef = [num / den_coef[0] for num in num_coef]
+    coef = [num / denom_coef[0] for num in numr_coef]
 
     return simplify_coefficients(coef, as_numr_denom)
 
 
 def truncation_error(stencil, interval=DEFAULT_INTERVAL):
-    xSet = create_coordinate_symbols(stencil, interval=interval)
+    x_set = create_coordinate_symbols(stencil, interval=interval)
 
     coef = coefficients(stencil)
 
-    num_expterm = len(xSet)
-    f_te = [taylor_series(x, num_expterm) for x in xSet]
+    num_term = len(x_set)
+    f_te = [taylor_series(x, num_term) for x in x_set]
 
-    eq = sum([coef[i] * f_te[i] for i in range(len(xSet))])
+    eq = sum([coef[i] * f_te[i] for i in range(len(x_set))])
 
-    return sp.simplify(
-        sp.symbols(DEFAULT_FUNCTION) - sp.nsimplify(eq, rational=True, tolerance=1e-10)
+    return sp.expand(
+        sp.simplify(
+            sp.symbols(DEFAULT_FUNCTION)
+            - sp.nsimplify(eq, rational=True, tolerance=1e-10)
+        )
     ).as_leading_term()
