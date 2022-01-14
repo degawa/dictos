77a9187229cbc9464e51dfe91008691702a683ea
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-01-14 16:22:04 +0900

    refactor finite_difference

--- dictos/finite_difference.py
+++ dictos/finite_difference.py
@@ -14,62 +14,58 @@ from .lagrangian_polynomial import lagrangian_poly, derivative
 from .taylor_expansion import taylor_series, derivative_symbol
 
 
-def getFiniteDifferenceEquation(
+def equation(
     stencil,
-    orderOfDifference=1,
-    intervalSymbolStr=DEFAULT_INTERVAL,
-    sameSubscriptsAsStencil=False,
+    deriv=1,
+    interval=DEFAULT_INTERVAL,
+    same_subscripts_as_stencil=False,
     evaluate=True,
 ):
-    xSet = create_coordinate_symbols(stencil, intervalSymbolStr)
-    fSet = create_function_symbols(xSet, DEFAULT_FUNCTION, sameSubscriptsAsStencil)
+    x_set = create_coordinate_symbols(stencil, interval)
+    f_set = create_function_symbols(x_set, DEFAULT_FUNCTION, same_subscripts_as_stencil)
 
     if evaluate:
-        coef = getFiniteDifferenceCoefficients(stencil, orderOfDifference)
+        coef = coefficients(stencil, deriv)
         eq = sp.simplify(
-            sum([coef[i] * fSet[i] for i in range(len(fSet))])
-            / sp.symbols(intervalSymbolStr) ** orderOfDifference
+            sum([coef[i] * f_set[i] for i in range(len(f_set))])
+            / sp.symbols(interval) ** deriv
         )
     else:
-        numr, denom = getFiniteDifferenceCoefficients(
-            stencil, orderOfDifference, as_numr_denom=True
-        )
+        numr, denom = coefficients(stencil, deriv, as_numr_denom=True)
         eq = div(
-            dot_product(numr, fSet),
-            denom * sp.symbols(intervalSymbolStr) ** orderOfDifference,
+            dot_product(numr, f_set),
+            denom * sp.symbols(interval) ** deriv,
         )
 
     return eq
 
 
-def getFiniteDifferenceCoefficients(stencil, orderOfDifference=1, as_numr_denom=False):
-    xSet = create_coordinate_symbols(stencil, DEFAULT_INTERVAL)
-    fSet = create_function_symbols(xSet, DEFAULT_FUNCTION)
+def coefficients(stencil, deriv=1, as_numr_denom=False):
+    x_set = create_coordinate_symbols(stencil, DEFAULT_INTERVAL)
+    f_set = create_function_symbols(x_set, DEFAULT_FUNCTION)
 
     x = sp.symbols(DEFAULT_INDEPENDENT_VARIABLE)
-    num, den = lagrangian_poly(x, xSet, fSet).as_numer_denom()
-    num_coef = num.as_poly(fSet).coeffs()
-    den_coef = den.as_poly(fSet).coeffs()
+    numr, denom = lagrangian_poly(x, x_set, f_set).as_numer_denom()
+    numr_coef = numr.as_poly(f_set).coeffs()
+    denom_coef = denom.as_poly(f_set).coeffs()
 
-    coef = [derivative(num / den_coef[0], x, orderOfDifference) for num in num_coef]
+    coef = [derivative(num / denom_coef[0], x, deriv) for num in numr_coef]
 
     return simplify_coefficients(coef, as_numr_denom)
 
 
-def getTruncationError(stencil, orderOfDifference, intervalSymbolStr=DEFAULT_INTERVAL):
-    xSet = create_coordinate_symbols(stencil, interval=intervalSymbolStr)
+def truncation_error(stencil, deriv, interval=DEFAULT_INTERVAL):
+    x_set = create_coordinate_symbols(stencil, interval=interval)
 
-    coef = getFiniteDifferenceCoefficients(xSet, orderOfDifference)
+    coef = coefficients(x_set, deriv)
 
-    num_expterm = len(xSet) + orderOfDifference
-    f_te = [taylor_series(x, num_expterm) for x in xSet]
+    num_term = len(x_set) + deriv
+    f_ts = [taylor_series(x, num_term) for x in x_set]
 
-    eq = sum([coef[i] * f_te[i] for i in range(len(xSet))])
+    eq = sum([coef[i] * f_ts[i] for i in range(len(x_set))])
 
-    intervalSymbol = sp.symbols(intervalSymbolStr)
+    h = sp.symbols(interval)
     return sp.simplify(
-        derivative_symbol(DEFAULT_FUNCTION, orderOfDifference)
-        - sp.nsimplify(
-            eq / intervalSymbol ** orderOfDifference, rational=True, tolerance=1e-10
-        )
+        derivative_symbol(DEFAULT_FUNCTION, deriv)
+        - sp.nsimplify(eq / h ** deriv, rational=True, tolerance=1e-10)
     ).as_leading_term()
