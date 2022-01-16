64170f906c7bffdd1a6153d88e230e06417acace
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-01-17 00:53:38 +0900

    add error that the stencil contains 0

--- dictos/error.py
+++ dictos/error.py
@@ -6,3 +6,20 @@ class StencilError(Exception):
     pass
 
 
+class ContainsZeroError(StencilError):
+    """
+    Exception raised for errors
+    that the stencil contains `0`.
+
+    Attributes:
+        message (str): Explanation of the error.
+    """
+
+    def __init__(self):
+        self.message = (
+            "The stencil contains `0`. "
+            "Remove `0` from the stencil using `.remove(0)` etc."
+        )
+
+    def __str__(self):
+        return self.message
