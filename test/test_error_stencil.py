55da6873e1af26f13596f6a5e8c6652b07f8ef09
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-01-17 21:50:03 +0900

    add duplicated points test

--- test/test_error_stencil.py
+++ test/test_error_stencil.py
@@ -6,7 +6,7 @@ sys.path.insert(1, "..")
 
 import unittest
 
-from dictos.utils import has_zero
+from dictos.utils import has_zero, has_duplicated_points
 from dictos.error.stencil import ContainsZeroError, DuplicatedPointError
 
 
@@ -20,6 +20,22 @@ class ErrorStencilTest(unittest.TestCase):
         stencil = [0, 1, 2]
         if has_zero(stencil):
             raise ContainsZeroError
+
+    @unittest.expectedFailure
+    def test_error_stencil_DuplicatedPointError(self):
+        """
+        test suite for error.stencil.DuplicatedPointError.
+        """
+
+        with self.subTest():
+            stencil = [0, 1, 2]
+            if has_duplicated_points(stencil):
+                raise DuplicatedPointError(stencil)
+
+        with self.subTest():
+            stencil = [1, 2, 1]
+            if has_duplicated_points(stencil):
+                raise DuplicatedPointError(stencil)
 
 
 if __name__ == "__main__":
