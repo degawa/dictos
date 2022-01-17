5132560df9403d6be52cb17274900ab2fd6ee288
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-01-17 20:09:32 +0900

    fix function name

--- dictos/error/utils.py
+++ dictos/error/utils.py
@@ -1,13 +1,13 @@
 import collections
 
 
-def extract_dupulicated_points(stencil: list) -> list:
-    """Extract duplicated points from the stencil.
+def find_duplicated_points(stencil: list) -> list:
+    """Find duplicated points from the stencil.
 
     Args:
         stencil (list of int or float): stencil which caused the error.
 
     Returns:
-        list of int or float: dupulicated points number.
+        list of int or float: duplicated points number.
     """
     return [point for point, count in collections.Counter(stencil).items() if count > 1]
