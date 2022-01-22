758a631c0c385ba703b4157a712a13743e7da5d5
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-01-22 12:02:19 +0900

    add test for exception

--- test/test_error_taylor_expansion.py
+++ test/test_error_taylor_expansion.py
@@ -7,7 +7,11 @@ sys.path.insert(1, "..")
 import unittest
 import sympy as sp
 
-from dictos.error.taylor_expansion import UnsupportedOrderOfDerivativeError
+from dictos.taylor_expansion import taylor_series, derivative_symbol
+from dictos.error.taylor_expansion import (
+    UnsupportedOrderOfDerivativeError,
+    NumberOfExpansionTermsIsNotNaturalNumberError,
+)
 
 
 class ErrorTaylorExpansionTest(unittest.TestCase):
@@ -21,6 +25,21 @@ class ErrorTaylorExpansionTest(unittest.TestCase):
         with self.subTest(deriv):
             if deriv < 0:
                 raise UnsupportedOrderOfDerivativeError(deriv)
+
+    def test_error_taylor_expansion_exception(self):
+        """
+        test suite for exceptions in taylor_expansion.
+        """
+
+        up_to = -1
+        with self.subTest("taylor_series with invalid up_to %d" % up_to):
+            with self.assertRaises(NumberOfExpansionTermsIsNotNaturalNumberError):
+                taylor_series("h", up_to)
+
+        deriv = -1
+        with self.subTest("symbols with invalid deriv %d" % deriv):
+            with self.assertRaises(UnsupportedOrderOfDerivativeError):
+                derivative_symbol("h", deriv)
 
 
 if __name__ == "__main__":
