c233cb1ef3b6f9af34a47f13ac7927ccb1ada8f5
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-01-23 00:36:52 +0900

    import version number in __init__

--- dictos/__init__.py
+++ dictos/__init__.py
@@ -1,3 +1,4 @@
+from .__version__ import __version__
 from .lagrangian_polynomial import lagrangian_basis, lagrangian_poly, derivative
 from .taylor_expansion import taylor_series
 from .finite_difference import (
